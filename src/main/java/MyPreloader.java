fdb493de4574dc0b7c5c3503b9c89af2706ecc80
wkq <1164129634@qq.com>
2020-02-10 13:03:16 +0800

    Preloader

--- src/main/java/MyPreloader.java
+++ src/main/java/MyPreloader.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import javafx.application.Preloader;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class MyPreloader {
+public class MyPreloader extends Preloader {
+    private Stage preStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.preStage=primaryStage;
+        Parent root= FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene=new Scene(root,200,80);
+        this.preStage.initStyle(StageStyle.UNDECORATED);
+        this.preStage.setResizable(false);
+        this.preStage.setScene(scene);
+        this.preStage.show();
+    }
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification info) {
+        if(info.getType()== StateChangeNotification.Type.BEFORE_START){
+            preStage.close();
+        }
+    }
 }
