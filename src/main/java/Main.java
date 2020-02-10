072ee649d50b9aaaffe2538442adac3045733d68
wkq <1164129634@qq.com>
2020-02-10 13:02:39 +0800

    更改Controller为Main,加入Preloader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,94 +1,166 @@
+import Controller.Controller;
+import Model.Data;
+import com.google.gson.Gson;
+import com.jfoenix.controls.JFXTextArea;
+import com.sun.javafx.application.LauncherImpl;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
 public class Main extends Application  {
+    @FXML
+    private Text updateTime;
+    @FXML
+    private Text dianosedAll;
+    @FXML
+    private Text suspectAll;
+    @FXML
+    private Text curedAll;
+    @FXML
+    private Text deathAll;
+    @FXML
+    private Text dianosedIncr;
+    @FXML
+    private Text suspectIncr;
+    @FXML
+    private Text curedIncr;
+    @FXML
+    private Text deathIncr;
+    @FXML
+    private Button updateEpidemic;
+    private Data data;
+    @Override
+    public void init() throws Exception {
+        Gson gson=new Gson();
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://tianqiapi.com/api").newBuilder();
+        urlBuilder.addQueryParameter("appid", "41135599")
+                .addQueryParameter("appsecret", "s4frVhQV")
+                .addQueryParameter("version", "epidemic");
+        Request request = new Request.Builder()
+                .url(urlBuilder.build())
+                .build();
+        Response response=client.newCall(request).execute();
+        data=gson.fromJson(response.body().string(), Data.class);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root=FXMLLoader.load(getClass().getResource("main.fxml"));
-        Scene scene=new Scene(root,200,80);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setResizable(false);
+        Parent root=FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene=new Scene(root,900,600);
         primaryStage.setScene(scene);
         primaryStage.show();
-        Thread thread=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try{
-                    Thread.sleep(2000);
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            try{
-                                Parent root=FXMLLoader.load(getClass().getResource("/sample.fxml"));
-                                Scene scene=new Scene(root,900,600);
-                                Stage newstage=new Stage();
-                                newstage.setScene(scene);
-                                primaryStage.close();
-                                newstage.show();
-                            }catch (IOException e){
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-        });
-        thread.start();
+        Text updateTime=(Text)root.lookup("#updateTime");
+        Text dianosedAll=(Text)root.lookup("#dianosedAll");
+        Text suspectAll=(Text)root.lookup("#suspectAll");
+        Text curedAll=(Text)root.lookup("#curedAll");
+        Text deathAll=(Text)root.lookup("#deathAll");
+        Text dianosedIncr=(Text)root.lookup("#dianosedIncr");
+        Text suspectIncr=(Text)root.lookup("#suspectIncr");
+        Text curedIncr=(Text)root.lookup("#curedIncr");
+        Text deathIncr=(Text)root.lookup("#deathIncr");
+        updateTime.setText("更新时间： "+data.getMessage().getDate());
+        dianosedAll.setText(data.getMessage().getDiagnosed()+"");
+        suspectAll.setText(data.getMessage().getSuspect()+"");
+        curedAll.setText(data.getMessage().getCured()+"");
+        deathAll.setText(data.getMessage().getDeath()+"");
+        dianosedIncr.setText(data.getMessage().getDiagnosedIncr()+"");
+        suspectIncr.setText(data.getMessage().getSuspectIncr()+"");
+        curedIncr.setText(data.getMessage().getCuredIncr()+"");
+        deathIncr.setText(data.getMessage().getDeathIncr()+"");
     }
 
 
     public static void main(String[] args){
-        launch(args);
+        LauncherImpl.launchApplication(Main.class,MyPreloader.class,args);
     }
 
+    public void updateEpidemic(ActionEvent event)throws IOException{
+        Gson gson=new Gson();
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://tianqiapi.com/api").newBuilder();
+        urlBuilder.addQueryParameter("appid", "41135599")
+                .addQueryParameter("appsecret", "s4frVhQV")
+                .addQueryParameter("version", "epidemic");
+        Request request = new Request.Builder()
+                .url(urlBuilder.build())
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
 
+            }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                data=gson.fromJson(response.body().string(), Data.class);
+                updateTime.setText("更新时间： "+data.getMessage().getDate());
+                dianosedAll.setText(data.getMessage().getDiagnosed()+"");
+                suspectAll.setText(data.getMessage().getSuspect()+"");
+                curedAll.setText(data.getMessage().getCured()+"");
+                deathAll.setText(data.getMessage().getDeath()+"");
+                dianosedIncr.setText(data.getMessage().getDiagnosedIncr()+"");
+                suspectIncr.setText(data.getMessage().getSuspectIncr()+"");
+                curedIncr.setText(data.getMessage().getCuredIncr()+"");
+                deathIncr.setText(data.getMessage().getDeathIncr()+"");
+            }
+        });
+    }
 
+    @FXML
+    public void updateMouseEntered(MouseEvent event){
+        Button button=(Button)event.getSource();
+        button.setStyle("-fx-background-color: #d3d3d3");
+    }
 
-    //test
-//        TextArea textArea=new TextArea();
-//        textArea.setWrapText(true);
-//        ScrollPane scrollPane=new ScrollPane();
-//        scrollPane.setContent(textArea);
-//        scrollPane.setFitToWidth(true);
-//        Scene scene=new Scene(scrollPane,500,500);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//
-//
-//
-//        try{
-//            OkHttpClient okHttpClient = new OkHttpClient().newBuilder().build();
-//            HttpUrl.Builder builder=HttpUrl.parse("https://tianqiapi.com/api").newBuilder();
-//            builder.addQueryParameter("appid","41135599")
-//                    .addQueryParameter("appsecret","s4frVhQV")
-//                    .addQueryParameter("version","epidemic");
-//            Request request=new Request.Builder().url(builder.build()).build();
-//            Response response = okHttpClient.newCall(request).execute();
-//            String message=decode(response.body().string());
-//            textArea.setText(message);
-//        }catch (IOException e){
-//
-//        }
+    @FXML
+    public void updateMouseExited(MouseEvent event){
+        Button button=(Button)event.getSource();
+        button.setStyle("-fx-background-color: #00000000");
+    }
+    @FXML
+    public void left(MouseEvent event){
+        Rectangle rectangle=(Rectangle)event.getSource();
+        rectangle.setHeight(250);
+        rectangle.setWidth(300);
+    }
+    @FXML
+    public void right(MouseEvent event){
+        Rectangle rectangle=(Rectangle)event.getSource();
+        rectangle.setWidth(200);
+        rectangle.setHeight(250);
+    }
+    @FXML void leftok(MouseEvent event){
+        Rectangle rectangle=(Rectangle)event.getSource();
+        rectangle.setHeight(200);
+        rectangle.setWidth(284);
+    }
+    @FXML
+    public void rightok(MouseEvent event){
+        Rectangle rectangle=(Rectangle)event.getSource();
+        rectangle.setHeight(200);
+        rectangle.setWidth(172);
+    }
 //    public static String decode(String unicodeStr) {
 //        if (unicodeStr == null) {
 //            return null;
